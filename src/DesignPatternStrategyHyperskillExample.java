de0eb931fa6b84c7da9f2c87a0f4724c6a1fe617
pwgtiluser.email <87475830+pwgtil@users.noreply.github.com>
2023-07-17 14:57:48 +0200

    feat: implement Strategy design pattern - Add Hyperskill example of this DP

---
+++ src/DesignPatternStrategyHyperskillExample.java
@@ -0,0 +1,87 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+class Finder {
+
+    private FindingStrategy strategy;
+
+    public Finder(FindingStrategy strategy) {
+        // write your code here
+        this.strategy = strategy;
+    }
+
+    /**
+     * It performs the search algorithm according to the given strategy
+     */
+    public int find(int[] numbers) {
+        return strategy.getResult(numbers);
+    }
+}
+
+interface FindingStrategy {
+
+    /**
+     * Returns search result
+     */
+    int getResult(int[] numbers);
+
+}
+
+class MaxFindingStrategy implements FindingStrategy {
+
+    public int getResult(int[] numbers) {
+        return IntStream.of(numbers).max().orElse(Integer.MIN_VALUE);
+    }
+}
+
+class MinFindingStrategy implements FindingStrategy {
+
+    public int getResult(int[] numbers) {
+        return IntStream.of(numbers).min().orElse(Integer.MAX_VALUE);
+    }
+}
+
+/* Do not change code below */
+class Main {
+
+    public static void main(String[] args) {
+
+        final Scanner scanner = new Scanner(System.in);
+
+        final String[] elements = scanner.nextLine().split("\\s+");
+        int[] numbers = null;
+
+        if ("EMPTY".equals(elements[0])) {
+            numbers = new int[0];
+        } else {
+            numbers = new int[elements.length];
+            for (int i = 0; i < elements.length; i++) {
+                numbers[i] = Integer.parseInt(elements[i]);
+            }
+        }
+
+        final String type = scanner.nextLine();
+
+        Finder finder = null;
+
+        switch (type) {
+            case "MIN":
+                finder = new Finder(new MinFindingStrategy());
+                break;
+            case "MAX":
+                finder = new Finder(new MaxFindingStrategy());
+                break;
+            default:
+                break;
+        }
+
+        if (finder == null) {
+            throw new RuntimeException(
+                    "Unknown strategy type passed. Please, write to the author of the problem.");
+        }
+
+        System.out.println(finder.find(numbers));
+    }
+}
